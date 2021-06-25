122b36ffb07f75543c9d75344b49b7a3f9df2369
Diego Núñez Silva <diego.nunez.mx@gmail.com>
2021-06-25 22:47:55 +0200

    Initialize code base

---
+++ src/main/java/com/dns/jvmmemorymetricsdemo/JvmMemoryMetricsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dns.jvmmemorymetricsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvmMemoryMetricsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvmMemoryMetricsDemoApplication.class, args);
+	}
+
+}
